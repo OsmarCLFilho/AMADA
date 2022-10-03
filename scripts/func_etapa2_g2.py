5d68c677867aa207415901d404ccc96f58d77107
jhorn26 <jeanfh26@gmail.com>
2022-10-02 22:02:14 -0300

    versão 3 referente a etapa 2 grupo 2

--- scripts/func_etapa2_g2.py
+++ scripts/func_etapa2_g2.py
@@ -1,14 +1,15 @@
 import pandas as pd
+import copy
 
 
-def concatenar_elementos_coluna(DataFrame, coluna: str, condicao = None):
+def concatenar_elementos_coluna(DataFrame: pd.DataFrame, coluna: str, condicao: str = None):
     dicionario = {}
     if condicao == None:
         indice = 'Todas'
         dicionario['Todas'] = ""
         anterior = None
         for line in range(len(DataFrame)):
-            dado = DataFrame.loc[line, coluna]
+            dado = DataFrame.loc[line, coluna].upper()
             if dado == anterior:
                 continue
             else:
@@ -17,74 +18,89 @@ def concatenar_elementos_coluna(DataFrame, coluna: str, condicao = None):
     else:
         for line in range(len(DataFrame)):
             indice = DataFrame.loc[line, condicao]
-            dado = DataFrame.loc[line, coluna]
+            dado = DataFrame.loc[line, coluna].upper()
             if dicionario.get(indice):
                 dicionario[indice] = dicionario[indice] + " " + dado
             else:
                 dicionario[indice] = dado
-    
+    for key in dicionario.keys():
+        dicionario[key] = dicionario[key].replace('\r\n', ' ').replace('_IGNORE_EMPTY_IGNORE_', ' ')
+        profundidade = 0
+        novo_texto = ""
+        for c in dicionario[key]:
+            if c == "[":
+                profundidade += 1
+            elif c == "]":
+                profundidade -= 1
+            elif profundidade == 0:
+                novo_texto += c
+        
+        dicionario[key] = novo_texto
+            
     return dicionario
 
 
-def concatenar_letras_album(DataFrame):
-    dicionario = {}
-    for line in range(len(DataFrame)):
-        indice = DataFrame.loc[line, 'album']
-        dado = DataFrame.loc[line, 'lyrics']
-        if dicionario.get(indice):
-            dicionario[indice] = dicionario[indice] + " " + dado
-        else:
-            dicionario[indice] = dado
-
-
-def remove_contracoes(string: str):
+def remove_contracoes(dicionario: dict):
     #contrações comuns no inglês
-    chars = ["'m","'s","'re","n't","'ll","'ve","'d"]
+    chars = ["'M","'S","'RE","N'T","'LL","'VE","'D"]
 
     #faz um loop com as contrações para retirar suas ocorrências
-    for str in chars:
-        string = string.replace(str, "")
+    for key in dicionario.keys():    
+        for str in chars:
+            dicionario[key] = dicionario[key].replace(str, "")
+        #devido a contração de "cannot" ser "can't" e não "cann't", ao retirar as contrações, "can't" ficará "ca"
+        dicionario[key] = dicionario[key].replace(' ca ', " can ")
 
-    #devido a contração de "cannot" ser "can't" e não "cann't", ao retirar as contrações, "can't" ficará "ca"
-    string = string.replace(' ca ', " can ")
-
-    return string
+    return dicionario
 
 
-def remove_caracteres(string: str):
+def remove_caracteres(dicionario: dict):
     #caracteres especiais que aparecem nos álbuns, nomes e letras das músicas
     chars = ["(",")","?","!","[","]",",",".","/","&","\"","'","-",":",";","“","”","‘","’","–","—"]
 
     #faz um loop com os caracteres para retirar suas ocorrências
-    for str in chars:
-        string = string.replace(str, "")
+    for key in dicionario.keys():    
+        for str in chars:
+            dicionario[key] = dicionario[key].replace(str, "")
 
-    return string
+    return dicionario
 
 
-def remove_irrelevantes(string: str):
+def remove_irrelevantes(dicionario: dict):
     with open("stopwords.txt") as file:
-        stopwords = file.read().split()
-
-    list_string = string.split()
-    list_aux = list_string.copy()
-
-    for str in list_aux:
-        if str in stopwords:
-            list_string.remove(str) 
+        stopwords = file.read().upper().split()
     
-    return list_string
+    for key in dicionario.keys():
+        dicionario[key] = dicionario[key].split()
+ 
+    dicionario_aux = copy.deepcopy(dicionario)
+
+    for key in dicionario.keys():     
+        for str in dicionario_aux[key]:
+            if str in stopwords:
+                dicionario[key].remove(str) 
+    
+    return dicionario
 
 
-def freq_elementos(list_string: list):
-    indice = list_string.unique()
+def freq_elementos(dicionario: dict):
+    for key in dicionario.keys():    
+        indice = list(set(dicionario[key]))
+        dados = []
 
-    dados = []
+        for ind in indice:
+            dados.append(dicionario[key].count(ind))
 
-    for ind in indice:
-        dados.append(list_string.count(ind))
+        serie = pd.Series(data=dados, index=indice, dtype='int64')
+        serie = serie.sort_values(ascending=False)
 
-    serie = pd.Series(data=dados, index=indice)
-    serie = serie.sort_values(ascending=False)
+        dicionario[key] = serie
 
-    return serie
+    return dicionario
+
+
+def relevancia(dicionario):
+    for key in dicionario.keys():
+        dicionario[key] = dicionario[key].count(key)
+    
+    return dicionario
