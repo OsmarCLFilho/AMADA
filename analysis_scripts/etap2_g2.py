51302bd07e4a4a6e51d137e7bd10a0151e9212e0
OsmarCLFilho <osmarclopesfilho@gmail.com>
2022-10-07 07:29:05 -0300

    functionized etap2_g2.py

--- analysis_scripts/etap2_g2.py
+++ analysis_scripts/etap2_g2.py
@@ -10,94 +10,87 @@ except FileNotFoundError:
     print("final_dataset.csv was not found in /results/. The file structure of the project is altered!")
     sys.exit(1)
 
-print("######  QUESTION 1  ######\n")
+def question_1():
+    print("######  QUESTION 1  ######\n")
 
-dictionary = fe.concat_elements_column(df, 'album')
-dictionary = fe.remove_contractions(dictionary)
-dictionary = fe.remove_characters(dictionary)
-dictionary = fe.elements_freq(dictionary)
+    dictionary = fe.concat_elements_column(df, 'album')
+    dictionary = fe.remove_contractions(dictionary)
+    dictionary = fe.remove_characters(dictionary)
+    dictionary = fe.elements_freq(dictionary)
 
-print("10 most common words in the album titles:\n")
-print(dictionary['all'].head(10))
+    print("10 most common words in the album titles:\n")
+    print(dictionary['all'].head(10))
 
-print("\n\n")
+def question_2():
+    print("######  QUESTION 2  ######\n")
 
+    dictionary = fe.concat_elements_column(df, 'name')
+    dictionary = fe.remove_contractions(dictionary)
+    dictionary = fe.remove_characters(dictionary)
+    dictionary = fe.elements_freq(dictionary)
 
-print("######  QUESTION 2  ######\n")
+    print("10 most common words in the track titles:\n")
+    print(dictionary['all'].head(10))
 
-dictionary = fe.concat_elements_column(df, 'name')
-dictionary = fe.remove_contractions(dictionary)
-dictionary = fe.remove_characters(dictionary)
-dictionary = fe.elements_freq(dictionary)
+def question_3():
+    print("######  QUESTION 3  ######\n")
 
-print("10 most common words in the track titles:\n")
-print(dictionary['all'].head(10))
+    dictionary = fe.concat_elements_column(df, 'lyrics', 'album')
+    dictionary = fe.remove_contractions(dictionary)
+    dictionary = fe.remove_characters(dictionary)
+    dictionary = fe.remove_undesirables(dictionary)
+    dictionary = fe.elements_freq(dictionary)
 
-print("\n\n")
+    print("10 most common words in the lyrics per album:\n")
 
+    for key in dictionary.keys():
+        print(key,":")
+        if isinstance(dictionary[key], str):
+            print(dictionary[key])
+        else:
+            print(dictionary[key].head(10))
+        print("\n")
 
-print("######  QUESTION 3  ######\n")
+def question_4():
+    print("######  QUESTION 4  ######\n")
 
-dictionary = fe.concat_elements_column(df, 'lyrics', 'album')
-dictionary = fe.remove_contractions(dictionary)
-dictionary = fe.remove_characters(dictionary)
-dictionary = fe.remove_undesirables(dictionary)
-dictionary = fe.elements_freq(dictionary)
+    dictionary = fe.concat_elements_column(df, 'lyrics')
+    dictionary = fe.remove_contractions(dictionary)
+    dictionary = fe.remove_characters(dictionary)
+    dictionary = fe.remove_undesirables(dictionary)
+    dictionary = fe.elements_freq(dictionary)
 
-print("10 most common words in the lyrics per album:\n")
+    print("10 most common words in the lyrics:\n")
+    print(dictionary['all'].head(10))
 
-for key in dictionary.keys():
-    print(key,":")
-    if isinstance(dictionary[key], str):
-        print(dictionary[key])
-    else:
-        print(dictionary[key].head(10))
-    print("\n")
+def question_5():
+    print("######  QUESTION 5  ######\n")
 
-print("\n\n")
+    dictionary = fe.concat_elements_column(df, 'lyrics', 'album')
+    dictionary = fe.relevancy(dictionary)
 
-print("######  QUESTION 4  ######\n")
+    print("Amount of times that the albums titles show up in the lyrics\n")
+    print("(albums whose lyrics were not found were removed):\n")
 
-dictionary = fe.concat_elements_column(df, 'lyrics')
-dictionary = fe.remove_contractions(dictionary)
-dictionary = fe.remove_characters(dictionary)
-dictionary = fe.remove_undesirables(dictionary)
-dictionary = fe.elements_freq(dictionary)
+    for key in copy.deepcopy(dictionary).keys():
+        if isinstance(dictionary[key], str):
+            dictionary.pop(key)
 
-print("10 most common words in the lyrics:\n")
-print(dictionary['all'].head(10))
+    for ind in sorted(dictionary, key = dictionary.get, reverse=True):
+        print(ind, ": ", dictionary[ind])
 
-print("\n\n")
+def question_6():
+    print("######  QUESTION 6  ######\n")
 
-print("######  QUESTION 5  ######\n")
+    dictionary = fe.concat_elements_column(df, 'lyrics', 'name')
+    dictionary = fe.relevancy(dictionary)
 
-dictionary = fe.concat_elements_column(df, 'lyrics', 'album')
-dictionary = fe.relevancy(dictionary)
+    print("Amount of times that the tracks titles show up in the lyrics\n")
+    print("(tracks whose lyrics were not found were removed):")
 
-print("Amount of times that the albums titles show up in the lyrics\n")
-print("(albums whose lyrics were not found were removed):\n")
+    for key in copy.deepcopy(dictionary).keys():
+        if isinstance(dictionary[key], str):
+            dictionary.pop(key)
 
-for key in copy.deepcopy(dictionary).keys():
-    if isinstance(dictionary[key], str):
-        dictionary.pop(key)
-
-for ind in sorted(dictionary, key = dictionary.get, reverse=True):
-    print(ind, ": ", dictionary[ind])
-
-print("\n\n")
-
-
-print("######  QUESTION 6  ######\n")
-
-dictionary = fe.concat_elements_column(df, 'lyrics', 'name')
-dictionary = fe.relevancy(dictionary)
-
-print("Amount of times that the tracks titles show up in the lyrics\n")
-print("(tracks whose lyrics were not found were removed):")
-
-for key in copy.deepcopy(dictionary).keys():
-    if isinstance(dictionary[key], str):
-        dictionary.pop(key)
-
-for ind in sorted(dictionary, key = dictionary.get, reverse=True):
-    print(ind, ": ", dictionary[ind])
+    for ind in sorted(dictionary, key = dictionary.get, reverse=True):
+        print(ind, ": ", dictionary[ind])
